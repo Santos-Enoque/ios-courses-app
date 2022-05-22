51b5d2cc8260ae7824d9c5ea85529ca5c31935e5
Santos Enoque <santosenoque.ss@gmail.com>
2022-05-22 19:21:44 +0900

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Santos Enoque on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
